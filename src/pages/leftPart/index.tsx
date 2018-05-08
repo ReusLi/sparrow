10072fa93ce7038439f774cf1a1c49f26503082d
ReusLi <574399934@qq.com>
2018-05-08 23:05:05 +0800

    click event will update inputFocusIndex

--- src/pages/leftPart/index.tsx
+++ src/pages/leftPart/index.tsx
@@ -6,7 +6,9 @@ import './index.css'
 
 class LeftPart extends React.Component<LeftPagesProps, LeftPagesStates> {
     /** 输入框的HTML元素集合 */
-    private inputHTMLElement: Array<any> = [];
+    private inputHTMLElements: Array<any> = [];
+
+    private InputComp: any;
 
     constructor(props: LeftPagesProps, state: LeftPagesStates) {
         super(props);
@@ -16,33 +18,39 @@ class LeftPart extends React.Component<LeftPagesProps, LeftPagesStates> {
             inputFocusIndex: 0
         };
     }
+    componentWillMount() {
+        this.InputComp = this.renderInputComponent();
+    }
 
     componentDidMount() {
-        this.inputHTMLElement[0].focus();
+        this.inputHTMLElements[0].focus();
     }
 
     public render() {
         const placeholder = this.state.placeholder;
-        const InputComp = this.renderInputComponent();
         return (
             <div>
-                {InputComp}
+                {this.InputComp}
             </div>
         )
     }
 
+    /** 
+     * 渲染输入框组件
+     */
     private renderInputComponent() {
-        const len = this.state.inputListNum.length;
+        const len: number = this.state.inputListNum.length + 10;
 
-        let result = [],
-            refVal = '';
+        let result: Array<any> = [],
+            refVal: string = '';
 
-        for (let index = 0; index < len + 10; index++) {
+        for (let index = 0; index < len; index++) {
             result.push(
                 <Row key={index} className="row-style">
                     <Input
-                        ref={(input) => { this.inputHTMLElement.push(input) }}
+                        ref={(input) => { this.inputHTMLElements.push(input) }}
                         placeholder={this.state.placeholder}
+                        onClick={this.onClick.bind(this, index)}
                         onPressEnter={this.onPressEnter.bind(this)}
                         onKeyDown={this.onKeyDown.bind(this)}
                     />
@@ -52,32 +60,50 @@ class LeftPart extends React.Component<LeftPagesProps, LeftPagesStates> {
 
         return result;
     }
-
+    /**
+     * 点击事件
+     * @param index 输入框序号
+     * @param e 事件对象
+     */
+    private onClick(index: number, e: any) {
+        // 点击input时,更新当前focus的input序号
+        this.setState({
+            inputFocusIndex: index
+        })
+    }
+    /**
+     * 回车事件
+     * @param e 事件对象
+     */
     private onPressEnter(e: object) {
         this.onKeyDown({
             keyCode: 40
         })
     }
-
+    /**
+     * 键盘按下事件
+     * @param e 事件对象
+     */
     private onKeyDown(e: any) {
         const key: number = e.keyCode,
-            maxLen: number = this.inputHTMLElement.length;
+            maxLen: number = this.inputHTMLElements.length;
 
         let focusIndex: number = this.state.inputFocusIndex;
 
         // 向上键
-        if (key === 38 && focusIndex >= 0) {
+        if (key === 38 && focusIndex > 0) {
             focusIndex--;
-        } 
+        }
         // 向下键
-        else if (key === 40 && focusIndex <= maxLen) {
+        else if (key === 40 && focusIndex < maxLen - 1) {
             focusIndex++;
-        } 
+        }
         // 其余情况返回
         else {
             return false;
         }
-        this.inputHTMLElement[focusIndex].focus();
+
+        this.inputHTMLElements[focusIndex].focus();
         this.setState({
             inputFocusIndex: focusIndex
         })
