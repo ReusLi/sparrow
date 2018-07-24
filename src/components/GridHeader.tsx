04b0f9a11984af75e698b00f1c959da8aa1c7faf
ReusLi <574399934@qq.com>
2018-07-24 23:00:29 +0800

    update code

--- src/components/GridHeader.tsx
+++ src/components/GridHeader.tsx
@@ -1,15 +1,23 @@
 import * as React from 'react'
 
+interface props {
+
+}
+
+interface states {
+    
+}
+
 export default class GridHeader extends React.Component {
-    // constructor(props: props, state: states) {
-    //     super(props);
-    // }
+    constructor(props: props, state: states) {
+        super(props);
+    }
 
     public render() {
         return (
             <div>
                 <table data-border={1}>
-                    <tbody>
+                    <thead>
                         <tr>
                             <th colSpan={2}>Company in USA</th>
                         </tr>
@@ -21,25 +29,7 @@ export default class GridHeader extends React.Component {
                             <td>Google, Inc.</td>
                             <td>1600 Amphitheatre Parkway Mountain View, CA 94043</td>
                         </tr>
-                    </tbody>
-                </table>
-
-                <table>
-                    <tbody>
-                        <tr>
-                            <th>月份</th>
-                            <th>存款</th>
-                            <th rowSpan={3}>假日存款</th>
-                        </tr>
-                        <tr>
-                            <td>一月</td>
-                            <td>3000 元</td>
-                        </tr>
-                        <tr>
-                            <td>二月</td>
-                            <td>5000 元</td>
-                        </tr>
-                    </tbody>
+                    </thead>
                 </table>
             </div>
         )
