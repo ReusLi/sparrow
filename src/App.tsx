36b6f759778c49ccbe08a9fe7c85b96dc1bc4518
ReusLi <574399934@qq.com>
2018-08-12 23:13:05 +0800

    更新注释

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,17 @@ export default class App extends React.Component {
     public render() {
         return (
             <Row>
+
+                {/* 操作cell的按钮组 */}
                 <Row className="kjax-handle-btn">
                     <ButtonGroup />
                 </Row>
+
+                {/* cell panel */}
                 <Row>
                     <Table />
                 </Row>
+                
             </Row>
         )
     }
