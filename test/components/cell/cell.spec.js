b4a62b149bd711aa7f21c286f1a90f79d452c6ff
ReusLi <574399934@qq.com>
2018-08-16 22:32:22 +0800

    补充完cell组件的必要单测

--- test/components/cell/cell.spec.js
+++ test/components/cell/cell.spec.js
@@ -3,36 +3,131 @@ import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { expect } from 'chai'
 
-import Cell from 'components/cell/cell'
 
+import Cell from 'components/cell/cell'
 Enzyme.configure({ adapter: new Adapter() });
 
-// 可能是shallow方法有问题
-const cellComponent = shallow(<Cell />).instance()
 
-describe('cell组件', () => {
-    let props = {
+let CellProps = {
+    // cell text
+    text: '',
+
+    // 单元格key
+    cellKey: {
+        X: 0,
+        Y: 0
+    },
+
+    // 是否可编辑
+    isEditable: false,
+
+    // 父组件选择的区域信息
+    selectInfo: {
         startCell: {
             X: 0,
             Y: 0
         },
         endCell: {
-            X: 7,
-            Y: 7
+            X: 0,
+            Y: 0
+        }
+    },
+
+    mouseDownEvent: function () { },
+
+    mouseUpEvent: function () { },
+
+    mouseOverEvent: function () { }
+}
+
+let cellComponent
+
+// 可能是shallow方法有问题
+try {
+    cellComponent = shallow(<Cell {...CellProps}/>).instance()
+} catch (e) {
+    console.log(e)
+}
+
+describe('cell组件', () => {
+    let props = {
+        selectInfo: {
+            startCell: {
+                X: 0,
+                Y: 0
+            },
+            endCell: {
+                X: 7,
+                Y: 7
+            }
         },
         cellKey: {
             X: 0,
             Y: 0
         }
     }
+
     describe('isInSideCell 函数能正确判断cell是否在矩阵内', () => {
-        it('it..', () => {
-            props.cellKey.X = 8
-            props.cellKey.Y = 0
+        it('在矩阵内的情况', () => {
+            props.cellKey.X = 4
+            props.cellKey.Y = 4
+
+            const result = cellComponent.isInSideCell(props)
+
+            expect(result).to.be.equal(true);
+        })
+
+        it('不在矩阵内的情况', () => {
+            props.cellKey.X = 9
+            props.cellKey.Y = 9
 
             const result = cellComponent.isInSideCell(props)
 
             expect(result).to.be.equal(false);
         })
+    })
+
+    describe('能否正确判断cell是否处于边界', () => {
+
+        it('isTop边界', () => {
+            props.cellKey.X = 0
+
+            let className = cellComponent.isTop([''], props)
+
+            className = className[1]
+
+            expect(className).to.be.equal('custom-focus-top');
+        })
+
+        it('isRight边界', () => {
+            props.cellKey.Y = 7
+
+            let className = cellComponent.isRight([''], props)
+
+            className = className[1]
+
+            expect(className).to.be.equal('custom-focus-right');
+        })
+
+        it('isBottm边界', () => {
+            props.cellKey.X = 7
+
+            let className = cellComponent.isBottm([''], props)
+
+            className = className[1]
+
+            expect(className).to.be.equal('custom-focus-bottom');
+        })
+
+        it('isLeft边界', () => {
+            props.cellKey.Y = 0
+
+            let className = cellComponent.isLeft([''], props)
+
+            className = className[1]
+
+            expect(className).to.be.equal('custom-focus-left');
+        })
+
     })
 })
