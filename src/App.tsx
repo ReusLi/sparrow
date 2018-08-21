0db136bbff618d4771fb69ba5df8ed3a594a44c1
ReusLi <574399934@qq.com>
2018-08-21 23:50:40 +0800

    动态构建矩阵

--- src/App.tsx
+++ src/App.tsx
@@ -6,43 +6,78 @@ import ButtonGroup from 'components/button-group/buttonGroup'
 import Table from 'components/table/table';
 
 import { CellKey } from 'cell/interface';
+import Cell from 'cell/cell';
 
 export default class App extends React.Component {
+    private row: number
+    private col: number
     private cellModels: Array<Array<CellKey>>
 
-    private buildCellKey () {
+    /**
+     * 通过2点确定矩阵左上角和右下角的坐标
+     */
+
+    /**
+     * 通过矩阵的左上角和右下角坐标
+     * 构建出矩阵内所有点的集合
+     */
+
+    /**
+     * 构建n*n的矩阵数据模型
+     * @param row 
+     * @param col 
+     * @param noUseCells 
+     */
+    private buildMatrixModel(row: number, col: number) {
+        let matrixModel: Array<Array<CellKey>> = [],
+            cellKey: CellKey
+
+        for (let i = 0; i < row; i++) {
+            matrixModel.push([])
+            for (let j = 0; j < col; j++) {
+                cellKey = {
+                    X: i,
+                    Y: j
+                }
+                matrixModel[i].push(cellKey)
+            }
+        }
+
+        return matrixModel;
+    }
+    private buildCellKey() {
         this.cellModels = [
             [
-                {X: 0, Y: 0},
-                {X: 0, Y: 1},
-                {X: 0, Y: 2},
-                {X: 0, Y: 3},
-                {X: 0, Y: 4},
-                {X: 0, Y: 5}
+                { X: 0, Y: 0 },
+                { X: 0, Y: 1 },
+                { X: 0, Y: 2 },
+                { X: 0, Y: 3 },
+                { X: 0, Y: 4 },
+                { X: 0, Y: 5 }
             ],
             [
-                {X: 1, Y: 0},
-                {X: 1, Y: 1, colSpan: 3, rowSpan: 2},
+                { X: 1, Y: 0 },
+                { X: 1, Y: 1, colSpan: 3, rowSpan: 2 },
                 // {X: 1, Y: 2},
                 // {X: 1, Y: 3},
-                {X: 1, Y: 4},
-                {X: 1, Y: 5}
+                { X: 1, Y: 4 },
+                { X: 1, Y: 5 }
             ],
             [
-                {X: 2, Y: 0},
+                { X: 2, Y: 0 },
                 // {X: 2, Y: 1},
                 // {X: 2, Y: 2},
                 // {X: 2, Y: 3},
-                {X: 2, Y: 4},
-                {X: 2, Y: 5}
+                { X: 2, Y: 4 },
+                { X: 2, Y: 5 }
             ],
             [
-                {X: 3, Y: 0},
-                {X: 3, Y: 1},
-                {X: 3, Y: 2},
-                {X: 3, Y: 3},
-                {X: 3, Y: 4},
-                {X: 3, Y: 5}
+                { X: 3, Y: 0 },
+                { X: 3, Y: 1 },
+                { X: 3, Y: 2 },
+                { X: 3, Y: 3 },
+                { X: 3, Y: 4 },
+                { X: 3, Y: 5 }
             ]
         ]
         return this.cellModels;
@@ -52,7 +87,8 @@ export default class App extends React.Component {
      * 第一次render前触发
      */
     componentWillMount() {
-        this.cellModels = this.buildCellKey()
+        // this.cellModels = this.buildCellKey()
+        this.cellModels = this.buildMatrixModel(10, 10)
     }
 
     public render() {
