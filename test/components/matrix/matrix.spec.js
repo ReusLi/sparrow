826a396990f4e30c20dd0b2ad06da7bab7ca854e
ReusLi <574399934@qq.com>
2018-09-07 00:09:00 +0800

    初步定好了matrix的单元测试

--- test/components/matrix/matrix.spec.js
+++ test/components/matrix/matrix.spec.js
@@ -1,26 +1,28 @@
-// import React from 'react';
-// import Enzyme, { shallow } from 'enzyme';
-// import Adapter from 'enzyme-adapter-react-16';
-// import { expect } from 'chai'
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { expect } from 'chai'
 
-// import App from '../../../src/App'
+import App from '../../../src/App'
 
-// Enzyme.configure({ adapter: new Adapter() });
+Enzyme.configure({ adapter: new Adapter() });
 
-// let matrixComponent
-// try {
-//     matrixComponent = mount(<App/>).instance()
-// } catch (e) {
-//     console.log(e)
-//     console.error('渲染matrix组件出错')
-// }
+let matrixComponent
+try {
+    matrixComponent = shallow(<App/>).instance()
+} catch (e) {
+    console.log(e)
+    console.error('渲染matrix组件出错')
+}
  
-// describe('matrix组件', () => {
+describe('matrix组件', () => {
 
-//     describe('测试正确渲染x行y列', () => {
-//         it('initTableHeader方法', () => {
-//             let cellList = matrixComponent.buildMatrixMergeCell(matrixModel, mergeCellList)
-//             expect(1).to.be.ok
-//         })
-//     })
-// })
+    describe('测试构建n*n矩阵的方法', () => {
+        it('buildMatrixMergeCell方法', () => {
+            let matrixModel = []
+            let mergeCellList = []
+            let cellList = matrixComponent.buildMatrixMergeCell(matrixModel, mergeCellList)
+            expect(1).to.be.ok
+        })
+    })
+})
