49e0feae48a96a75f3b181b47b883f8847bf0df4
ReusLi <574399934@qq.com>
2018-09-09 22:56:52 +0800

    app的props和states

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,38 @@ import * as React from 'react'
 
 import Matrix from 'components/matrix/matrix'
 
-export default class App extends React.Component {
+import Parse5 from 'utils/parse5'
 
-    constructor() {
-        super({});
+interface AppState {
+
+}
+
+interface AppProps {
+
+}
+
+export default class App extends React.Component<AppProps, AppState> {
+
+    constructor(props: AppProps, state: AppState) {
+        super(props);
     }
     
+    componentWillMount () {
+        this.paser5spec()
+    }
+
+    /**
+     * 测试Parse5模块
+     */
+    paser5spec() {
+        let cls = new Parse5()
+        
+        let r1 = cls.parse('<!DOCTYPE html><html><head></head><body>Hi there!</body></html>')
+        let r2 = cls.serialize(r1)
+
+        console.log(r1)
+        console.log(r2)
+    }
 
     public render() {
         return (
