f8ca9d178b8ee6f4b78f39337e4ba4e6c2473a22
ReusLi <574399934@qq.com>
2018-09-09 22:41:38 +0800

    修改Matrix单元测试的引用路径

--- test/components/matrix/matrix.spec.js
+++ test/components/matrix/matrix.spec.js
@@ -3,7 +3,7 @@ import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { expect } from 'chai'
 
-import App from '../../../src/App'
+import App from 'components/matrix/matrix'
 
 Enzyme.configure({ adapter: new Adapter() });
 
