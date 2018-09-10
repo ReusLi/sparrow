f426ce92a61d3c3dd191306f8bd1fe24c3629a55
ReusLi <574399934@qq.com>
2018-09-10 21:41:58 +0800

    隐藏parse5模块, 暂时不需要用到

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import * as React from 'react'
 
 import Matrix from 'components/matrix/matrix'
 
-import Parse5 from 'utils/parse5'
-
 interface AppState {
 
 }
@@ -17,22 +15,9 @@ export default class App extends React.Component<AppProps, AppState> {
     constructor(props: AppProps, state: AppState) {
         super(props);
     }
-    
-    componentWillMount () {
-        this.paser5spec()
-    }
 
-    /**
-     * 测试Parse5模块
-     */
-    paser5spec() {
-        let cls = new Parse5()
-        
-        let r1 = cls.parse('<!DOCTYPE html><html><head></head><body>Hi there!</body></html>')
-        let r2 = cls.serialize(r1)
+    componentWillMount() {
 
-        console.log(r1)
-        console.log(r2)
     }
 
     public render() {
--- src/utils/parse5.js
+++ src/utils/parse5.js
@@ -13,4 +13,33 @@ export default class Parse5 {
 
         return htmlTemplate;
     }
+
+    /**
+     * 测试Parse5模块
+     */
+    paser5spec() {
+        let cls = new Parse5()
+
+        // 获取dom的树结构
+        let r1 = cls.parse('<!DOCTYPE html><html><head></head><body>Hi there!</body></html>')
+
+        // 替换树结构属性, 在Body标签上新增属性
+        r1.childNodes[1].childNodes[1].attrs = [
+            {
+                name: "data-at",
+                value: "bd"
+            },
+            {
+                name: "style",
+                value: "color: red"
+            }
+        ]
+
+        // 序列号
+        let r2 = cls.serialize(r1)
+
+        // 输出
+        console.log(r1)
+        console.log(r2)
+    }
 }
