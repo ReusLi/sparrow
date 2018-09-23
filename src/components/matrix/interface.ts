804ba5705f1f364075d50174f438d4b05169eb28
ReusLi <574399934@qq.com>
2018-09-24 00:08:15 +0800

    补充martrix interface注释

--- src/components/matrix/interface.ts
+++ src/components/matrix/interface.ts
@@ -4,6 +4,12 @@ export interface CellKey extends CellKey { }
 
 export interface SelectInfo extends SelectInfo { }
 
+/**
+ * 矩阵状态
+ * row 矩阵行数
+ * col 矩阵列数
+ * cellModels 矩阵内单元格模型 是一个 row * col 的 二维数组
+ */
 export interface MatrixState {
     row: number,
     col: number,
