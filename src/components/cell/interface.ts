32573da99662f510915f5227d0af9f8d5b884be6
ReusLi <574399934@qq.com>
2018-09-24 00:10:28 +0800

    补充table interface 注释

--- src/components/cell/interface.ts
+++ src/components/cell/interface.ts
@@ -17,6 +17,7 @@ export interface CellProps {
     // 父组件选择的区域信息
     selectInfo: SelectInfo,
 
+    // mouse 系列事件, 单元格每个状态都会通知table 以更新选中状态
     mouseDownEvent: Function,
 
     mouseUpEvent: Function,
--- src/components/matrix/interface.ts
+++ src/components/matrix/interface.ts
@@ -4,15 +4,14 @@ export interface CellKey extends CellKey { }
 
 export interface SelectInfo extends SelectInfo { }
 
-/**
- * 矩阵状态
- * row 矩阵行数
- * col 矩阵列数
- * cellModels 矩阵内单元格模型 是一个 row * col 的 二维数组
- */
 export interface MatrixState {
+    // row 矩阵行数
     row: number,
+
+    // col 矩阵列数
     col: number,
+
+    // cellModels 矩阵内单元格模型 是一个 row * col 的 二维数组
     cellModels: Array<Array<CellKey>>
 }
 
