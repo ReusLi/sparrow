36611b00cf49754322609d0f31a0ae5c1b3d4df0
ReusLi <574399934@qq.com>
2018-11-13 22:01:42 +0800

    add mobx  to button group componenet

--- src/components/button-group/buttonGroup.tsx
+++ src/components/button-group/buttonGroup.tsx
@@ -2,12 +2,19 @@ import * as React from 'react'
 
 import { Button, Col, Row } from 'antd';
 
+// mobx
+import MatrixMobx from 'state/matrix/matrix'
+
 export default class ButtonGroup extends React.Component {
     public render() {
         return (
             <Row>
                 <Col span={4}>
-                    <Button>合并</Button>
+                    <Button
+                        onMouseUp={MatrixMobx.updateMatrixModel.bind(MatrixMobx)}
+                    >
+                        合并
+                    </Button>
                 </Col>
                 <Col span={4}>
                     <Button>拆分</Button>
