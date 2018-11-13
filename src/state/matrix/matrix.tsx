8e0bcb7ccfbfd243b0eeb464bcee3ddf1e5ec821
ReusLi <574399934@qq.com>
2018-11-13 22:05:13 +0800

    add matrix mobx comment

--- src/state/matrix/matrix.tsx
+++ src/state/matrix/matrix.tsx
@@ -46,10 +46,19 @@ class MatrixMobx {
     this.cellModels = cellModels
   }
 
+  /**
+   * cell组件触发mouse down事件时, 会通过context通知 matrix组件
+   * 记录下mouse down cell信息
+   * @param cellKey 
+   */
   onCellMouseDown(cellKey: CellKey) {
     this.mouseDownCell = cellKey
   }
 
+  /**
+   * cell组件mouse up事件时
+   * @param cellKey 
+   */
   onCellMouseUp(cellKey: CellKey) {
     // 如果是点击一个单元格 不需要做处理
     if (util.isSameCellKey(this.mouseDownCell, cellKey)) {
@@ -58,14 +67,17 @@ class MatrixMobx {
 
     this.mouseUpCell = cellKey
     let SelectInfo: SelectInfo = MatrixUtils.buildXY(this.mouseDownCell, this.mouseUpCell)
-    
-    
-    let {hideCellList, kc}: {hideCellList: Array<CellKey>, kc: CellKey} = util.getSkipCellByCellKeys(SelectInfo.startCell, SelectInfo.endCell)
+
+
+    let { hideCellList, kc }: { hideCellList: Array<CellKey>, kc: CellKey } = util.getSkipCellByCellKeys(SelectInfo.startCell, SelectInfo.endCell)
 
     this.mergeCellList.push(kc)
     this.hideCellList = this.hideCellList.concat(hideCellList)
   }
 
+  /**
+   * 更新矩阵模型
+   */
   updateMatrixModel() {
     const row = this.row,
       col = this.col;
