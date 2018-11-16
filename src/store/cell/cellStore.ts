75c5238c150cbb83c4cf74f3eead6d4dc432772d
ReusLi <574399934@qq.com>
2018-11-16 22:38:19 +0800

    update cell store

--- src/store/cell/cellStore.ts
+++ src/store/cell/cellStore.ts
@@ -1,9 +1,16 @@
 import { computed, observable, action } from "mobx"
 
 import { CellKey, SelectInfo } from 'interface/common'
+import Cell from "components/cell/cell";
 
 class cellStore {
-    
+    isEditable: boolean = true
+
+    selectInfo: SelectInfo
+
+    mouseDownPoint: CellKey
+
+
 }
 
 export default new cellStore()
