fbd04cc148e0e7c02f781dc3bafa4942f34b0dd7
ReusLi <574399934@qq.com>
2018-11-24 23:48:10 +0800

    删掉无用的代码

--- src/components/button-group/buttonGroup.tsx
+++ src/components/button-group/buttonGroup.tsx
@@ -5,9 +5,6 @@ import { Button, Col, Row } from 'antd';
 // matrixStore mobx
 import matrixStore from 'store/matrix/matrixStore'
 
-// matrixStore mobx
-import cellStore from 'store/cell/cellStore'
-
 export default class ButtonGroup extends React.Component {
     public render() {
         return (
