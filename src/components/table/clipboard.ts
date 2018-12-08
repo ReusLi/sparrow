17530a583699cd5a4b3fb487043725b6e6b68b00
ReusLi <574399934@qq.com>
2018-12-08 20:55:51 +0800

    update code

--- src/components/table/clipboard.ts
+++ src/components/table/clipboard.ts
@@ -5,9 +5,14 @@ class clipboard {
      */
     private pasteData: Array<Array<string>>
 
+    /**
+     * 监听ctrl + v事件
+     * @param dom 需要监听paste事件的dom
+     */
     bindClipEvent(dom: HTMLElement) {
         dom.addEventListener('paste', (event: any) => {
             let data = event.clipboardData.getData('text')
+            console.log(data)
             data = data.split('\n')
             data = data.map((item: string) => {
                 return item.split(/\s+/)
