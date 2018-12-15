51a8beeef802862b77285d2f5cb9bf6318d49090
ReusLi <574399934@qq.com>
2018-12-16 00:29:26 +0800

    解决tableComponent单测报错的情况

--- test/components/table/table.spec.js
+++ test/components/table/table.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { expect } from 'chai'
 
@@ -8,22 +8,23 @@ import tableConst from './const.tsx'
 
 Enzyme.configure({ adapter: new Adapter() });
 
-// let tableComponent
-// try {
-//     tableComponent = mount(<Table cellModels={tableConst.tabelProps.cellModels}/>).instance()
-// } catch (e) {
-//     console.log(e)
-//     console.error('渲染Table组件出错')
-// }
+let tableComponent
+try {
+    tableComponent = shallow(<Table cellModels={tableConst.tabelProps.cellModels}/>).instance()
+} catch (e) {
+    console.log(e)
+    console.error('渲染Table组件出错')
+}
  
-// describe('table组件', () => {
-//     describe('测试正确渲染x行y列', () => {
-//         it('initTableHeader方法', () => {
-//             // TypeError: Cannot convert a Symbol value to a string
-//             // 好像是karma对es6的语法有兼容问题, 可以试试配置karma.config 或 webpack.config的label ?
-//             let cellList = tableComponent.initTableHeader(tableConst.tabelProps.cellModels)
-//             console.log(cellList)
-//             expect(1).to.be.ok
-//         })
-//     })
-// })
+describe('table组件', () => {
+    describe('测试正确渲染x行y列', () => {
+        it('initTableHeader方法', () => {
+            // 如果console.log关于tableComponent, cellList的信息
+            // 就会报错: 
+            // TypeError: Cannot convert a Symbol value to a string
+            // 这里应该写成 console.log(JSON.stringify(cellList)) 的形式
+            let cellList = tableComponent.initTableHeader(tableConst.tabelProps.cellModels)
+            expect(cellList.length).to.be.equal(4)
+        })
+    })
+})
