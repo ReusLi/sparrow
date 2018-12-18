e038070436a9a2ceef93b219a33a8286ea57e3a0
ReusLi <574399934@qq.com>
2018-12-18 23:15:16 +0800

    add row add col

--- src/components/button-group/buttonGroup.tsx
+++ src/components/button-group/buttonGroup.tsx
@@ -6,6 +6,7 @@ import { Button, Col, Row } from 'antd';
 import matrixStore from 'store/matrix/matrixStore'
 
 import antdTableStore from 'store/antdTableStore'
+import { CellKey } from 'components/matrix/interface';
 
 export default class ButtonGroup extends React.Component {
     public render() {
@@ -23,6 +24,18 @@ export default class ButtonGroup extends React.Component {
                         onMouseUp={matrixStore.disMergeCell.bind(matrixStore)}
                     >拆分</Button>
                 </Col>
+
+                <Col span={4}>
+                    <Button
+                        onMouseUp={this.addRow.bind(this)}
+                    >加行</Button>
+                </Col>
+
+                <Col span={4}>
+                    <Button
+                        onMouseUp={this.addCol.bind(this)}
+                    >加列</Button>
+                </Col>
                 <Col span={4}>
                     <Button
                         onMouseUp={antdTableStore.syncTableColumns.bind(antdTableStore)}
@@ -30,5 +43,46 @@ export default class ButtonGroup extends React.Component {
                 </Col>
             </Row>
         )
+    }
+
+    private addRow() {
+        let cellModels = matrixStore.cellModels,
+            newRow: Array<CellKey> = []
+
+        const X = cellModels[0].length - 1
+
+        cellModels[0].forEach((item, index) => {
+            const cell: CellKey = {
+                X: X,
+                Y: index,
+                rowSpan: 1,
+                colSpan: 1,
+                isHide: false,
+                text: `未命名`
+            }
+            newRow.push(cell)
+        })
+
+        cellModels.push(newRow)
+
+        matrixStore.setCellModels(cellModels)
+    }
+
+    private addCol() {
+        let cellModels = matrixStore.cellModels
+
+        cellModels.map((row, index) => {
+            const cell: CellKey = {
+                X: index,
+                Y: row.length,
+                rowSpan: 1,
+                colSpan: 1,
+                isHide: false,
+                text: `未命名`
+            }
+            row.push(cell)
+        })
+
+        matrixStore.setCellModels(cellModels)
     }
 }
