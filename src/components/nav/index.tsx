608d5be3ba6355fa7f4e9f14e7bfd65288d5dac1
ReusLi <574399934@qq.com>
2019-08-27 23:05:08 +0800

    add useEffect

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react'
 import './index.less';
 
-import { Menu, Icon } from 'antd';
+import { Menu, Icon, Alert } from 'antd';
 import * as PropTypes from "prop-types";
 
-const buildSubMenu = (subMenu: Array<subMenu>) => {
+
+const buildSubMenu = function (subMenu: Array<subMenu>) {
     return subMenu.map(menu => {
         return (
             <Menu.Item key={menu.id}>
@@ -15,12 +16,22 @@ const buildSubMenu = (subMenu: Array<subMenu>) => {
 }
 
 export function Nav ({ subMenu }: PropTypes.InferProps<navProps>) {
-    console.log(subMenu)
-
     const SubMenu = buildSubMenu(subMenu)
 
+    // 声明一个新的叫做 “count” 的 state 变量
+    const [count, setMenu] = React.useState(0);
+
+    React.useEffect(() => {
+        // Update the document title using the browser API
+        document.title = `You clicked ${count} times`;
+    });
+
+    const handleClick = () => {
+        setMenu(count + 1)
+    }
+
     return (
-        <Menu mode="horizontal">
+        <Menu mode="horizontal" onClick={handleClick}>
             {SubMenu}
         </Menu>
     )
