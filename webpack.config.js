a9f0770767686c6d28f96fea8c36f0b0230ded92
ReusLi <574399934@qq.com>
2019-08-30 22:09:59 +0800

    add webpack file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -53,13 +53,26 @@ module.exports = {
                     use: [
                         { loader: "css-loader" },
                         {
-                            loader: "less-loader", 
+                            loader: "less-loader",
                             options: {
                                 javascriptEnabled: true
                             }
                         }
                     ]
                 })
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            bypassOnDebug: true, // webpack@1.x
+                            disable: true, // webpack@2.x and newer
+                        },
+                    },
+                ],
             }
         ]
     },
