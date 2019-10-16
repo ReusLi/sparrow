44917a5616c67dc41fbf788f260c3071ac2810cf
ReusLi <574399934@qq.com>
2019-10-16 22:41:06 +0800

    reset file enter

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,21 @@ import AntdTable from 'components/antd-table'
 
 import { Row, Col } from 'antd'
 
+import Home from 'pages/home'
+
 export default class App extends React.Component {
 
     public render() {
         return (
-            <Row gutter={20}>
-                <Col span={12}>
-                    <Matrix />
-                </Col>
-                <Col span={12}>
-                    <AntdTable />
-                </Col>
-            </Row>
+            // <Row gutter={20}>
+            //     <Col span={12}>
+            //         <Matrix />
+            //     </Col>
+            //     <Col span={12}>
+            //         <AntdTable />
+            //     </Col>
+            // </Row>
+            <Home></Home>
         )
     }
 }
