a1b0d18768a2eb86794a8de2bfb9aab4e4001918
ReusLi <574399934@qq.com>
2019-12-11 22:57:02 +0800

    add code

--- src/components/entry-icon/index.tsx
+++ src/components/entry-icon/index.tsx
@@ -3,11 +3,17 @@ import './index.less';
 
 import { Row, Icon } from 'antd';
 
+const openUrl = (url: string) => {
+    window.open(url);
+}
 export default class Home extends React.Component {
+
     public render () {
         return (
             <Row className="entry-icon">
-                <Icon type="notification" />
+                <Icon type="notification" onClick={() => {
+                    openUrl('https://www.baidu.com')
+                }} />
                 <Icon type="question-circle" />
                 <Icon type="edit" />
             </Row>
