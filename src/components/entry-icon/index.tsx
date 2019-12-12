80696f0b528b4f6446e919f3e1820d37d12d99ca
ReusLi <574399934@qq.com>
2019-12-12 22:44:55 +0800

    add code

--- src/components/entry-icon/index.tsx
+++ src/components/entry-icon/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import './index.less';
 
-import { Row, Icon, Tooltip } from 'antd';
+import { Row, Col, Icon, Tooltip } from 'antd';
 
 const openUrl = (url: string) => {
     window.open(url);
@@ -10,18 +10,23 @@ export default class Home extends React.Component {
 
     public render () {
         return (
-            <Row className="entry-icon">
-                <Tooltip title="message">
-                    <Icon type="notification" onClick={() => {
-                        openUrl('https://www.baidu.com')
-                    }} />
-                </Tooltip>
-                <Tooltip title="question">
-                    <Icon type="question-circle" />
-                </Tooltip>
-                <Tooltip title="feedback">
-                    <Icon type="edit" />
-                </Tooltip>
+            <Row className="entry-icon" type="flex" justify="space-between">
+                <Col>
+                    <Tooltip title="message">
+                        <Icon type="notification" onClick={() => {
+                            openUrl('https://www.baidu.com')
+                        }} />
+                    </Tooltip>
+                    <Tooltip title="question">
+                        <Icon type="question-circle" />
+                    </Tooltip>
+                    <Tooltip title="feedback">
+                        <Icon type="edit" />
+                    </Tooltip>
+                </Col>
+                <Col>
+                    <Icon type="github" className="user-icon"/>
+                </Col>
             </Row>
             // <TableGroup tableList={tableList} />
         )
