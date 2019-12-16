0117b7d9d5ff847fd4b6d15fc652d074bd34037a
ReusLi <574399934@qq.com>
2019-12-16 21:23:04 +0800

    add code

--- src/pages/welcome/banner/index.tsx
+++ src/pages/welcome/banner/index.tsx
@@ -2,27 +2,9 @@ import * as React from 'react'
 
 import { Layout } from 'antd'
 
-import { observer } from 'mobx-react'
-
-import { Nav } from 'components/nav'
-
-import { TableGroup } from 'components/table-radio-group'
-
-const subMenu = [
-    {
-        id: 1,
-        text: '首页'
-    }, {
-        id: 2,
-        text: '文档'
-    }, {
-        id: 3,
-        text: '帮助'
-    }
-]
 export function Banner () {
 
     return (
-        <div>123</div>
+        <div></div>
     )
 }
