a870d806922528f6a24c6519f4aba618c3f3cfd2
ReusLi <574399934@qq.com>
2019-12-19 09:52:45 +0800

    add code

--- src/pages/welcome/banner/index.tsx
+++ src/pages/welcome/banner/index.tsx
@@ -1,16 +1,10 @@
 import * as React from 'react'
 
-import { Layout } from 'antd'
-
 import './index.less';
 
-const sss = require('../../../asstes/images/conversion_tips_ecommerce.jpg')
 export function Banner () {
 
     return (
-        // <div>
-        //     <img className="banner-image" src={sss}></img>
-        // </div>
         <div className="banner-image"></div>
     )
 }
